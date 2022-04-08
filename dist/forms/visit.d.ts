2aa702b76ccab2c100bf147db7ff4445bf6baa68
John Gile <me@jgile.com>
2022-04-08 07:59:31 -0700

    wip

---
+++ dist/forms/visit.d.ts
@@ -0,0 +1,25 @@
+export default function visit(method: any, url: any, data: any, options: any, requestOptions: any): {
+    submit: (method: import("./types").Method, href: any, options?: object, requestOptions?: object) => Promise<import("axios").AxiosResponse<any, any>>;
+    data: object;
+    errors: {};
+    response: {};
+    hasErrors: boolean;
+    processing: boolean;
+    progress: null;
+    wasSuccessful: boolean;
+    recentlySuccessful: boolean;
+    isDirty: boolean;
+    setOptions(options?: any): void;
+    setRequestOptions(options?: any): void;
+    setData(key: any, value: any): void;
+    transform(callback: any): void;
+    setDefaults(key: any, value: any): void;
+    reset(...fields: any): void;
+    setError(key: any, value: any): void;
+    clearErrors(...fields: any): void;
+    get(url: any, options?: any, requestOptions?: any): Promise<import("axios").AxiosResponse<any, any>>;
+    post(url: any, options?: any, requestOptions?: any): Promise<import("axios").AxiosResponse<any, any>>;
+    put(url: any, options?: any, requestOptions?: any): Promise<import("axios").AxiosResponse<any, any>>;
+    patch(url: any, options?: any, requestOptions?: any): Promise<import("axios").AxiosResponse<any, any>>;
+    delete(url: any, options?: any, requestOptions?: any): Promise<import("axios").AxiosResponse<any, any>>;
+};
