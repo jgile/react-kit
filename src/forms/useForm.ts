5e53e3768dfdfae06704a53ea0d48094e7e6b4d1
John Gile <me@jgile.com>
2023-04-16 10:40:30 -0700

    bump

--- src/forms/useForm.ts
+++ src/forms/useForm.ts
@@ -9,6 +9,34 @@ import {AxiosError} from 'axios';
 import {objectToFormData} from "./formData";
 import {hasFiles} from "./files";
 
+export type UseFormObject = {
+    submit: (requestOptions?: AxiosRequestConfig, options?: VisitParams) => Promise<unknown>;
+    data: any;
+    delete(requestOptions?: Partial<AxiosRequestConfig>, options?: Partial<VisitParams>): Promise<unknown>;
+    put(requestOptions?: Partial<AxiosRequestConfig>, options?: Partial<VisitParams>): Promise<unknown>;
+    patch(requestOptions?: Partial<AxiosRequestConfig>, options?: Partial<VisitParams>): Promise<unknown>;
+    transform(callback: any): void;
+    isDirty: boolean;
+    post(requestOptions?: Partial<AxiosRequestConfig>, options?: Partial<VisitParams>): Promise<unknown>;
+    bindField(name: string, defaultValue?: any): { onChange: (value: any) => void; name: string; value: any[string] };
+    get(requestOptions?: Partial<AxiosRequestConfig>, options?: Partial<VisitParams>): Promise<unknown>;
+    setOptions(options?: any): void;
+    getData(key: string, defaultValue?: any): any;
+    recentlySuccessful: boolean;
+    hasErrors: boolean;
+    setRequestOptions(options?: any): void;
+    setData: (key: (((data: any) => any) | string | any), value?: any) => void;
+    clearErrors(...fields: any): void;
+    wasSuccessful: boolean;
+    setDefaults(key: any, value: any): void;
+    response: Partial<AxiosResponse<any, any>>;
+    setError(key: any, value: any): void;
+    processing: boolean;
+    progress: any;
+    reset(...fields: any): void;
+    errors: {}
+};
+
 export function isFormData(payload: RequestPayload): payload is FormData {
     return typeof payload === 'object' && payload instanceof FormData;
 }
@@ -17,7 +45,7 @@ export default function useForm<Args extends RequestPayload, S extends VisitPara
     args: Args = {} as Args,
     requestOptions: R = {} as R,
     formOptions: S = {} as S
-) {
+): UseFormObject {
     const isMounted = useRef(null)
     const [defaults, setDefaults] = useState(args)
     const [data, setData] = useState<Args>(args);
@@ -142,7 +170,7 @@ export default function useForm<Args extends RequestPayload, S extends VisitPara
         });
     }, [data, defaultOptions, defaultRequestOptions]);
 
-    const setDataFunction = (key: ((data: any) => Args) | string | Args, value?: any) => {
+    const setDataFunction = (key: ((data: any) => Args) | string | Args, value?: any): void => {
         let state = {...data};
         if (typeof key === 'string') {
             if (value && value.target && typeof value.target.value !== 'undefined') {
